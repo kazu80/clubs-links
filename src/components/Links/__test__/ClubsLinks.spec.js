9d7598d29a16d81d486a95a97285dd41d4ac97d3
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2023-04-16 09:34:54 +0900

    Add test by vitest for v1.0.x

--- src/components/Links/__test__/ClubsLinks.spec.js
+++ src/components/Links/__test__/ClubsLinks.spec.js
@@ -1,7 +1,7 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect } from 'vitest'
 
-import { mount } from '@vue/test-utils';
-import ClubsLinks from '../ClubsLinks.vue';
+import { mount } from '@vue/test-utils'
+import ClubsLinks from '../ClubsLinks.vue'
 
 describe('ClubsLinks Test', () => {
 	it('v1.0.X', () => {
@@ -15,18 +15,20 @@ describe('ClubsLinks Test', () => {
 			],
 		}
 
-		const wrapper = mount(ClubsLinks, {props: {options: [linksOptions]}});
+		const wrapper = mount(ClubsLinks, { props: { options: [linksOptions] } })
 
 		// タイトルがある
-		expect(wrapper.text()).toContain('Links');
+		expect(wrapper.text()).toContain('Links')
 
 		// 4つのリンクがある
-		expect(wrapper.findAll('a').length).toBe(4);
+		expect(wrapper.findAll('a').length).toBe(4)
 
 		// 1つめのリンクのタイトルがlinkOptionsの1つめのタイトルと一致する
-		expect(wrapper.findAll('a').at(0).text()).toBe(linksOptions.value[0].title);
+		expect(wrapper.findAll('a').at(0).text()).toBe(linksOptions.value[0].title)
 
 		// 1つめのリンクのURLがlinkOptionsの1つめのURLと一致する
-		expect(wrapper.findAll('a').at(0).attributes('href')).toBe(linksOptions.value[0].url);
-	});
-});
+		expect(wrapper.findAll('a').at(0).attributes('href')).toBe(
+			linksOptions.value[0].url
+		)
+	})
+})
